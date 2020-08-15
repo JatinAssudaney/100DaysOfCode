e947caff579a5686c08a5c9979cffad2b22045ca
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-08-16 02:22:26 +0530

    Seasons React App

--- 5. Modern React with Redux/3. Class Components/seasons/src/index.js
+++ 5. Modern React with Redux/3. Class Components/seasons/src/index.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends Component {
-  constructor(props) {
-    super(props); // required for using Component Parent Class
-    this.state = { lat: null, errorMessage: "" };
+  // Due to babel.js this is same as previous implementation using constructor
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        // To UPDATE state we called setState
         this.setState({ lat: position.coords.latitude });
-
-        // WE DID NOT DO : this.state.lat = position.coords.latitude
       },
       (err) => {
         this.setState({ errorMessage: err.message });
@@ -18,15 +18,19 @@ class App extends Component {
     );
   }
 
-  // React says we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude : {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading!</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  // React says we have to define render!!
+  render() {
+    return this.renderContent();
   }
 }
 
