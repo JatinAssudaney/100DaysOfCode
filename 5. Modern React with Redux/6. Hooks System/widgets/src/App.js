716c3e5416f09a53ed241799a882d3703ea4be77
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-08-18 02:35:00 +0530

    Accordion & Search Widgets

--- 5. Modern React with Redux/6. Hooks System/widgets/src/App.js
+++ 5. Modern React with Redux/6. Hooks System/widgets/src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+
+const items = [
+  {
+    title: "What is React",
+    content:
+      "React is an open-source JavaScript library for building user interfaces or UI components.",
+  },
+  {
+    title: "Why use React",
+    content:
+      "It's used for handling the view layer for web and mobile apps. React also allows us to create reusable UI components",
+  },
+  {
+    title: "How do you use React",
+    content: "You use React by creating components",
+  },
+];
 
 export default function App() {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 }
---
+++ 5. Modern React with Redux/6. Hooks System/widgets/src/components/Search.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 500);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          format: "json",
+          origin: "*",
+          srsearch: debouncedTerm,
+        },
+      });
+      setResults(data.query.search);
+    };
+    search();
+  }, [debouncedTerm]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            GO
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            type="text"
+            className="input"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
+
+export default Search;
