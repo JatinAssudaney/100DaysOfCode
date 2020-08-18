901f73f0ce124ae362865938c0ce3ccf8745cb84
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-08-19 03:38:40 +0530

    Widgets App

--- 5. Modern React with Redux/6. Hooks System/widgets/src/App.js
+++ 5. Modern React with Redux/6. Hooks System/widgets/src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -37,26 +39,28 @@ const options = [
 ];
 
 export default function App() {
-  /* const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
         <Dropdown
           label="Select a Color"
           options={options}
           selected={selected}
           onSelectedChange={setSelected}
         />
-      ) : null}
-    </div>
-  ); */
-  return (
-    <div>
-      <Translate />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 }
