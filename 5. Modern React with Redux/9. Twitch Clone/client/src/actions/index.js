f2d4c3f5e2493b6fe070d527bfe81b0b8a08612f
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-09-02 02:17:50 +0530

    Editing Stream Component

--- 5. Modern React with Redux/9. Twitch Clone/client/src/actions/index.js
+++ 5. Modern React with Redux/9. Twitch Clone/client/src/actions/index.js
@@ -50,8 +50,9 @@ export const fetchStream = (id) => {
 
 export const editStream = (id, formValues) => {
   return async (dispatch) => {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
     dispatch({ type: EDIT_STREAM, payload: response.data });
+    history.push("/");
   };
 };
 
--- 5. Modern React with Redux/9. Twitch Clone/client/src/components/streams/StreamEdit.js
+++ 5. Modern React with Redux/9. Twitch Clone/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,38 @@
-import React from "react";
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+export class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={_.pick(this.props.stream, "title", "description")}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+const mapDispatchToProps = { fetchStream, editStream };
+
+export default connect(mapStateToProps, mapDispatchToProps)(StreamEdit);
