de08a5a8eee622e8c1fcb650b3b222603cc8742d
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-09-02 16:47:20 +0530

    Twitch Clone App

--- 5. Modern React with Redux/9. Twitch Clone/client/src/components/streams/StreamShow.js
+++ 5. Modern React with Redux/9. Twitch Clone/client/src/components/streams/StreamShow.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading</div>;
@@ -13,6 +43,7 @@ class StreamShow extends Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls></video>
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
