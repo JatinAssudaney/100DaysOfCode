a0b2dab0b1beccb62e1f555a3061ce67031f3d07
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-09-02 16:07:18 +0530

    Twitch Clone Frontend Completed

--- 5. Modern React with Redux/9. Twitch Clone/client/src/components/streams/StreamList.js
+++ 5. Modern React with Redux/9. Twitch Clone/client/src/components/streams/StreamList.js
@@ -33,7 +33,9 @@ class StreamList extends Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera"></i>
           <div className="content">
-            {stream.title}
+            <Link className="header" to={`/streams/show/${stream.id}`}>
+              {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
