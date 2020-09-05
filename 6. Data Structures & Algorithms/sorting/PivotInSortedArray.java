c382072abc2b9a2fae4d1e47447d2132b5b76766
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-09-05 21:58:40 +0530

    Binary Search Application

---
+++ 6. Data Structures & Algorithms/sorting/PivotInSortedArray.java
@@ -0,0 +1,31 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class PivotInSortedArray {
+    public static int findPivot(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] < arr[high]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return arr[high];
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int pivot = findPivot(arr);
+        System.out.println(pivot);
+        scn.close();
+    }
+}
