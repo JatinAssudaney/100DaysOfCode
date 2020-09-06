39fc7efc6fb3ecd013b361c3c202f05946617b9c
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-09-07 03:20:20 +0530

    Graphs DFS

---
+++ 6. Data Structures & Algorithms/graphs/GetConnectedComponents.java
@@ -0,0 +1,60 @@
+package graphs;
+
+import java.io.*;
+import java.util.*;
+
+public class GetConnectedComponents {
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            int wt = Integer.parseInt(parts[2]);
+            graph[v1].add(new Edge(v1, v2, wt));
+            graph[v2].add(new Edge(v2, v1, wt));
+        }
+        boolean[] visited = new boolean[vtces];
+
+        ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
+        for (int v = 0; v < vtces; v++) {
+            if (!visited[v]) {
+                ArrayList<Integer> comp = new ArrayList<>();
+                getConnectedComponent(graph, v, visited, comp);
+                comps.add(comp);
+            }
+        }
+        System.out.println(comps);
+    }
+
+    private static void getConnectedComponent(ArrayList<Edge>[] graph, int src, boolean[] visited,
+            ArrayList<Integer> comp) {
+        visited[src] = true;
+        comp.add(src);
+        for (Edge edge : graph[src]) {
+            if (!visited[edge.nbr]) {
+                getConnectedComponent(graph, edge.nbr, visited, comp);
+            }
+        }
+    }
+}
