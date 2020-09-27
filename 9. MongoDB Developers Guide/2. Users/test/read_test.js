0dd452326e609d5ecb067e538b64f129e9d4ee4c
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-09-28 00:29:21 +0530

    Pagination Enabled using skip and limit

--- 9. MongoDB Developers Guide/2. Users/test/read_test.js
+++ 9. MongoDB Developers Guide/2. Users/test/read_test.js
@@ -2,10 +2,15 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading users out of the database", () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach(async () => {
-    joe = await new User({ name: "Joe" }).save();
+    joe = new User({ name: "Joe" });
+    alex = new User({ name: "Alex" });
+    maria = new User({ name: "Maria" });
+    zach = new User({ name: "Zach" });
+
+    await Promise.all([joe.save(), alex.save(), maria.save(), zach.save()]);
   });
 
   it("Finds All users with a name of joe", async () => {
@@ -16,5 +21,12 @@ describe("Reading users out of the database", () => {
   it("Finds a user with a particular id", async () => {
     const user = await User.findById(joe._id);
     assert(user.name === "Joe");
+  });
+
+  it("Can skip and limit the result set for Pagination", async () => {
+    const users = await User.find({}).sort({ name: 1 }).skip(1).limit(2);
+    assert(users.length == 2);
+    assert(users[0].name === "Joe");
+    assert(users[1].name === "Maria");
   });
 });
