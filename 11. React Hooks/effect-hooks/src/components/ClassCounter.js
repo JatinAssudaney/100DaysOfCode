6fe01b4aab5c5cdad3d0f9a8489acb8d7aa9de50
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-15 18:13:16 +0530

    Conditional Render Class Lifecycle Method

--- 11. React Hooks/effect-hooks/src/components/ClassCounter.js
+++ 11. React Hooks/effect-hooks/src/components/ClassCounter.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
 
 export default class ClassCounter extends Component {
-  state = { count: 0 };
+  state = { count: 0, name: "" };
 
   componentDidMount() {
     document.title = `Clicked ${this.state.count} times`;
   }
 
   componentDidUpdate(prevProps, prevState) {
-    document.title = `Clicked ${this.state.count} times`;
+    if (prevState.count !== this.state.count) {
+      console.log("Updating Document Title");
+      document.title = `Clicked ${this.state.count} times`;
+    }
   }
   render() {
     return (
       <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
           Click {this.state.count} times
         </button>
