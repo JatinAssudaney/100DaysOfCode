452ed2eea13f5d4700a9042052dd43cd3f5d143a
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-15 18:29:11 +0530

    Run only Once Class Lifecycle Method

--- 11. React Hooks/effect-hooks/src/App.js
+++ 11. React Hooks/effect-hooks/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 // import ClassCounter from "./components/ClassCounter";
-import UseEffectCounter from "./components/UseEffectCounter";
+// import UseEffectCounter from "./components/UseEffectCounter";
+import ClassMouse from "./components/ClassMouse";
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
-      <UseEffectCounter />
+      {/* <UseEffectCounter /> */}
+      <ClassMouse />
     </div>
   );
 }
