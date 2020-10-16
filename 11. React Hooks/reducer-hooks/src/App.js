73465054ec9de7f291e39c3ad4542ad9220dcbae
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-16 10:11:52 +0530

    useReducer with useContext Initial Setup

--- 11. React Hooks/reducer-hooks/src/App.js
+++ 11. React Hooks/reducer-hooks/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
-import MultipleUseReducer from "./components/MultipleUseReducer";
+import ComponentA from "./components/useReducerWithContext/ComponentA";
+import ComponentB from "./components/useReducerWithContext/ComponentB";
+import ComponentC from "./components/useReducerWithContext/ComponentC";
+// import MultipleUseReducer from "./components/MultipleUseReducer";
 // import Counter from "./components/Counter";
 // import CustomCounter from "./components/CustomCounter";
 
@@ -9,7 +12,11 @@ function App() {
     <div className="App">
       {/* <Counter /> */}
       {/* <CustomCounter /> */}
-      <MultipleUseReducer />
+      {/* <MultipleUseReducer /> */}
+
+      <ComponentA />
+      <ComponentB />
+      <ComponentC />
     </div>
   );
 }
---
+++ 11. React Hooks/reducer-hooks/src/components/useReducerWithContext/ComponentF.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ComponentF() {
+  return <div></div>;
+}
+
+export default ComponentF;
