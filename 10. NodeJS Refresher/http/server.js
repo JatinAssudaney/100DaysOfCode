2c44010bce18be0800bf1549cdf51acde5567658
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-11-03 15:10:56 +0530

    HTTP Status Code

--- 10. NodeJS Refresher/http/server.js
+++ 10. NodeJS Refresher/http/server.js
@@ -1,8 +1,37 @@
 const http = require("http");
 
+const todos = [
+  {
+    id: 1,
+    text: "Todo One",
+  },
+  {
+    id: 2,
+    text: "Todo Two",
+  },
+  {
+    id: 3,
+    text: "Todo Three",
+  },
+];
+
 const server = http.createServer((req, res) => {
-  console.log(req.method);
-  res.end();
+  // res.statusCode = 404;
+  // res.setHeader("Content-Type", "application/json");
+  // res.setHeader("X-Powered-By", "Node.js");
+
+  res.writeHead(404, {
+    "Content-Type": "application/json",
+    "X-Powered-By": "Node.js",
+  });
+
+  res.end(
+    JSON.stringify({
+      success: false,
+      error: "Not Found",
+      data: null,
+    })
+  );
 });
 
 const PORT = 5000;
