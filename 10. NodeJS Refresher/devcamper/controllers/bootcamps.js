dc832a6ee37ccd317217307a0e9988e6d61512af
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-11-03 21:29:26 +0530

    Adding routing with express: Clean Way

---
+++ 10. NodeJS Refresher/devcamper/controllers/bootcamps.js
@@ -0,0 +1,40 @@
+// @desc    Get all Bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Show all Bootcamps" });
+};
+
+// @desc    Get single Bootcamps
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show Bootcamp ${req.params.id} ` });
+};
+
+// @desc    Create a Bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Create new Bootcamp" });
+};
+
+// @desc    Update a Bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update Bootcamp ${req.params.id}` });
+};
+
+// @desc    Delete a Bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
+};
