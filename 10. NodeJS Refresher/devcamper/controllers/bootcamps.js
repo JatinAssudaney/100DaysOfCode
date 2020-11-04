cf5b0008dbbe5786d9269b02d11683ebba01aad0
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-11-05 01:52:16 +0530

    Bootcamps CRUD Operation using Mongoose and Express

--- 10. NodeJS Refresher/devcamper/controllers/bootcamps.js
+++ 10. NodeJS Refresher/devcamper/controllers/bootcamps.js
@@ -1,40 +1,118 @@
+const Bootcamp = require("../models/Bootcamp");
+
 // @desc    Get all Bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all Bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({
+      success: true,
+      count: bootcamps.length,
+      data: bootcamps,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // @desc    Get single Bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show Bootcamp ${req.params.id} ` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const bootcamp = await Bootcamp.findById(id);
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false,
+        message: "Data not found with the particular ID",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // @desc    Create a Bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Create new Bootcamp" });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // @desc    Update a Bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update Bootcamp ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false,
+        message: "Data not found with the particular ID",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 // @desc    Delete a Bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const bootcamp = await Bootcamp.findByIdAndDelete(id);
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false,
+        message: "Data not found with the particular ID",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
--- 10. NodeJS Refresher/devcamper/server.js
+++ 10. NodeJS Refresher/devcamper/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// DB Connection
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+// Body Parser
+app.use(express.json());
 
 // Dev loggin middleware
 if (process.env.NODE_ENV === "development") {
@@ -22,5 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
