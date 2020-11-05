f16fa31a3965655d3a57055b525beee844dfd2b4
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-11-05 05:46:24 +0530

    Mongoose Middlewares

--- 10. NodeJS Refresher/devcamper/models/Bootcamp.js
+++ 10. NodeJS Refresher/devcamper/models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 const { Schema } = mongoose;
 
 const BootcampSchema = new Schema({
@@ -97,6 +98,12 @@ const BootcampSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
