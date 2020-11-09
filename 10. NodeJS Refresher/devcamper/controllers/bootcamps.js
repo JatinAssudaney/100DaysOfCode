c7a99b8caf776d4c08d089ad70586f1e330a696f
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-11-10 02:01:00 +0530

    Referencing Bootcamp and User

--- 10. NodeJS Refresher/devcamper/controllers/bootcamps.js
+++ 10. NodeJS Refresher/devcamper/controllers/bootcamps.js
@@ -32,6 +32,22 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req.body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamps
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If user is not an admin
+  if (publishedBootcamp && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
 
   res.status(201).json({
--- 10. NodeJS Refresher/devcamper/models/Bootcamp.js
+++ 10. NodeJS Refresher/devcamper/models/Bootcamp.js
@@ -100,6 +100,11 @@ const BootcampSchema = new Schema(
       type: Date,
       default: Date.now,
     },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   {
     toJSON: { virtuals: true },
