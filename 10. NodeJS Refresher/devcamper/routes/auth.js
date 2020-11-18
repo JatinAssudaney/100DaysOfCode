a132c2e8ad69a274dc2a314f830ca658d56ed751
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-11-18 07:32:48 +0530

    Update User Details

--- 10. NodeJS Refresher/devcamper/routes/auth.js
+++ 10. NodeJS Refresher/devcamper/routes/auth.js
@@ -5,6 +5,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 
@@ -13,6 +15,8 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resettoken", resetPassword);
 
